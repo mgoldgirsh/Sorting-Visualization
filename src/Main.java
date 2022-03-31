7ab4858e164d5778a6645f2ad05e6dee64f1e7e6
Matthew Goldgirsh <mgoldgirsh@gmail.com>
2022-03-31 01:12:15 -0400

    added animations

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import java.awt.BorderLayout;
 import javax.swing.JFrame;
 
+// the main class for Sort Panel
+// runs the GUI visualization
 public class Main {
-
   public static void main(String[] args) {
-
     JFrame f = new JFrame("Sort Simulation");
     f.setLayout(new BorderLayout());
     SortPanel p = new SortPanel();
